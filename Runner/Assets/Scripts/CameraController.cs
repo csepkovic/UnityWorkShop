9e4afaae01af21ea28801886c6ab3617fad1d93d
Colin Sepkovic <uberdude858@gmail.com>
2018-07-24 14:08:51 -0700

    Uploaded project day 2

---
+++ Runner/Assets/Scripts/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public GameObject floorCube;
+    public GameObject player;
+    public Vector3 playerCamera;
+    public float end;
+    // Use this for initialization
+    void Start()
+    {
+        for (int i = 0; i < 150; i++)
+        {
+            GameObject obj = (GameObject)Instantiate(Resources.Load("Floor Block"), new Vector3(i, 0, 0), Quaternion.identity);
+            end = i;
+        }
+        player = GameObject.FindGameObjectsWithTag("Player")[0];
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        playerCamera = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z-10);
+        transform.position = playerCamera;
+	}
+}
---
+++ Runner/Assets/Scripts/FloorBlock.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorBlock : MonoBehaviour {
+    Rigidbody2D rb2D;
+    CameraController cameraController;
+	// Use this for initialization
+	void Start () {
+        rb2D = this.GetComponent<Rigidbody2D>();
+        cameraController = GameObject.FindWithTag("MainCamera").GetComponent<CameraController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.y < -10){
+            cameraController.end++;
+            transform.rotation = new Quaternion(0, 0, 0, 0);
+            transform.position = new Vector3(cameraController.end, 0, 0);
+            rb2D.gravityScale = 0f;
+            rb2D.velocity = Vector2.zero;
+        }
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+	{
+        if (collision.gameObject.tag == "Light"){
+            rb2D.gravityScale = 1.5f;
+            Debug.Log("collision with light");
+        }
+	}
+}
